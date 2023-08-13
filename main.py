3d9e664750cfdd7542c33ac7eee68c6841655270
Pythonesa <pythonesamar@gmail.com>
2023-08-13 18:46:19 -0300

    ✨ enhance edit task functionality

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     root.geometry("800x600")
     main_frame = ttk.Frame(root)
     main_frame.pack(pady=20, padx=20, fill="both", expand=True)
-    main_frame.grid_rowconfigure(0, weight=1)  # Permite que task_frame se expanda
+    main_frame.grid_rowconfigure(0, weight=1)
     main_frame.grid_columnconfigure(0, weight=1)
 
     #List of tasks:
@@ -33,7 +33,7 @@ def main():
     add_task_button = ttk.Button(buttons_frame, text="Añadir tarea", command=lambda:add_task(root))
     add_task_button.pack(side=tk.LEFT, padx=20)
     #Edit task:
-    edit_task_button = ttk.Button(buttons_frame, text="Editar tarea", command=edit_task)
+    edit_task_button = ttk.Button(buttons_frame, text="Editar tarea", command=lambda:edit_task(root))
     edit_task_button.pack(side=tk.LEFT, padx=20)
     #Delete task:
     delete_task_button = ttk.Button(buttons_frame, text="Eliminar tarea", command=delete_task)
@@ -57,19 +57,32 @@ def add_task(parent):
         else:
             messagebox.showinfo("Error", "El nombre de la tarea no puede estar vacío!")
 
-def edit_task():
+def edit_task(parent):
     try:
-        selected_task_index = task_list.curselection()[0]
-        current_task = task_list.get(selected_task_index)
-        new_task = simpledialog.askstring("Editar Tarea", "Edita la tarea:")
-    
-        if new_task:
-            task_list.delete(selected_task_index)
-            task_list.insert(selected_task_index, new_task)
-        else:
-            messagebox.showinfo("Error", "La tarea no puede quedar vacía!")
-    except IndexError:
-        messagebox.showinfo("Error", "No hay ninguna tarea seleccionada!")
+        selected_task_index = task_list.curselection()
+        if not selected_task_index:
+            messagebox.showinfo("Error", "No hay ninguna tarea seleccionada!")
+            return
+        task_name = task_list.get(selected_task_index[0])
+        task_data = tasks[task_list.get(selected_task_index[0])]
+        task_description = task_data['description']
+        task_creation_date = task_data['creation_date']
+        task_due_date = task_data['due_date']
+        dialog = TaskDialog(parent, "Editar tarea", task_name, task_description, task_creation_date, task_due_date)
+        if dialog.result:
+            updated_task_name, updated_task_description, updated_task_creation_date, updated_task_due_date = dialog.result
+            if updated_task_name != task_name:
+                del tasks[task_name]
+            tasks[updated_task_name] = {
+                'description': updated_task_description,
+                'creation_date': task_creation_date,
+                'due_date': updated_task_due_date
+            }
+            task_list.delete(selected_task_index[0])
+            task_list.insert(selected_task_index[0], updated_task_name)
+    except KeyError:
+        messagebox.showerror("Error", "La tarea seleccionada no fue encontrada!")
+        
 
 def delete_task():
     try:
