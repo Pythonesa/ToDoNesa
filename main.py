5879619684227cbc5a062de81cea232b3050d9b9
Pythonesa <pythonesamar@gmail.com>
2023-08-13 16:27:36 -0300

    💄 change to grid for best UI

--- main.py
+++ main.py
@@ -4,27 +4,35 @@ from tkinter import ttk, simpledialog, messagebox
 task_list = None
 
 def main():
+    global task_list
+    
     #Main window:
     root = tk.Tk()
     root.title("ToDoNesa")
     root.geometry("800x600")
-    
+    main_frame = ttk.Frame(root)
+    main_frame.pack(pady=20, padx=20, fill="both", expand=True)
+    main_frame.grid_rowconfigure(0, weight=1)  # Permite que task_frame se expanda
+    main_frame.grid_columnconfigure(0, weight=1)
+
     #List of tasks:
-    global task_list
-    task_frame = ttk.Frame(root)
-    task_frame.pack(pady=20, padx=20, fill="both", expand=True)
+    task_frame = ttk.Frame(main_frame)
+    task_frame.grid(row=0, column=0, sticky="nsew")
     scrollbar = ttk.Scrollbar(task_frame)
     scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
     task_list = tk.Listbox(task_frame, yscrollcommand=scrollbar.set, selectmode=tk.SINGLE)
     task_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
     scrollbar.config(command=task_list.yview)
+    #Frame for buttons:
+    buttons_frame = ttk.Frame(main_frame)
+    buttons_frame.grid(row=1, column=0, pady=10, sticky="ew")
     
     #Add task:
-    add_task_button = ttk.Button(root, text="Añadir tarea", command=add_task)
-    add_task_button.pack(pady=20)
+    add_task_button = ttk.Button(buttons_frame, text="Añadir tarea", command=add_task)
+    add_task_button.pack(side=tk.LEFT, padx=20)
     #Edit task:
-    edit_task_button = ttk.Button(root, text="Editar tarea", command=edit_task)
-    edit_task_button.pack(pady=20)
+    edit_task_button = ttk.Button(buttons_frame, text="Editar tarea", command=edit_task)
+    edit_task_button.pack(side=tk.LEFT, padx=20)
     
     #Main loop:
     root.mainloop()
