25523b8da37b48bb68e4b68044836dd96f9a79c0
Pythonesa <pythonesamar@gmail.com>
2023-08-15 20:28:14 -0300

    :sparkles: add task priority

--- dialogs/task_dialog.py
+++ dialogs/task_dialog.py
@@ -4,8 +4,9 @@ from datetime import datetime
 from tkcalendar import DateEntry
 
 class TaskDialog(simpledialog.Dialog):
-    def __init__(self, parent, title, task_name="", task_description="", task_creation_date="", task_due_date=""):
+    def __init__(self, parent, title, task_name="", task_priority="Baja", task_description="", task_creation_date="", task_due_date=""):
         self.default_task_name = task_name
+        self.default_task_priority = task_priority
         self.default_task_description = task_description
         self.default_task_creation_date = task_creation_date
         self.default_task_due_date = task_due_date
@@ -13,11 +14,14 @@ class TaskDialog(simpledialog.Dialog):
     
     def body(self, parent):
         ttk.Label(parent, text="Tarea:").grid(column=0, row=0, sticky="w")
-        ttk.Label(parent, text="Descripción:").grid(column=0, row=1, sticky="w")
-        ttk.Label(parent, text="Fecha de creación:").grid(column=0, row=2, sticky="w")
-        ttk.Label(parent, text="Fecha límite:").grid(column=0, row=3, sticky="w")
+        ttk.Label(parent, text="Prioridad:").grid(column=0, row=1, sticky="w")
+        ttk.Label(parent, text="Descripción:").grid(column=0, row=2, sticky="w")
+        ttk.Label(parent, text="Fecha de creación:").grid(column=0, row=3, sticky="w")
+        ttk.Label(parent, text="Fecha límite:").grid(column=0, row=4, sticky="w")
         
         self.e1 = ttk.Entry(parent)
+        self.priority_options = ["Alta", "Media", "Baja"]
+        self.priority_combobox = ttk.Combobox(parent, values=self.priority_options, state="readonly")
         self.e2 = ttk.Entry(parent)
         self.e3 = ttk.Entry(parent)
         self.e4 = DateEntry(parent, date_pattern="dd/mm/Y")
@@ -25,15 +29,17 @@ class TaskDialog(simpledialog.Dialog):
         
         
         self.e1.grid(column=1, row=0, sticky="ew")
-        self.e2.grid(column=1, row=1, sticky="ew")
-        self.e3.grid(column=1, row=2, sticky="ew")
-        self.e4.grid(column=1, row=3, sticky="ew")
+        self.priority_combobox.grid(column=1, row=1, sticky="ew")
+        self.e2.grid(column=1, row=2, sticky="ew")
+        self.e3.grid(column=1, row=3, sticky="ew")
+        self.e4.grid(column=1, row=4, sticky="ew")
         
         self.due_date_var = tk.BooleanVar(value=bool(self.default_task_due_date))
         self.due_date_checkbox = ttk.Checkbutton(parent, text="Establecer fecha límite", variable=self.due_date_var, command=self.toggle_due_date)
         self.due_date_checkbox.grid(column=0, row=4, sticky="w")
         
         self.e1.insert(0, self.default_task_name)
+        self.priority_combobox.set(self.default_task_priority)
         self.e2.insert(0, self.default_task_description)
         self.e3.insert(0, self.default_task_creation_date or datetime.today().strftime("%d/%m/%Y"))
         self.e4.insert(0, self.default_task_due_date or "")
@@ -52,8 +58,9 @@ class TaskDialog(simpledialog.Dialog):
     
     def apply(self):
         task_name = self.e1.get()
+        task_priority = self.priority_combobox.get()
         task_description = self.e2.get()
         task_creation_date = self.e3.get()
         task_due_date = self.e4.get() if self.due_date_var.get() else None
         
-        self.result = (task_name, task_description, task_creation_date, task_due_date)
+        self.result = (task_name, task_priority, task_description, task_creation_date, task_due_date)
--- main.py
+++ main.py
@@ -46,10 +46,11 @@ def main():
 def add_task(parent):
     dialog = TaskDialog(parent, "Crear nueva tarea")
     if dialog.result:
-        task_name, task_description, task_creation_date, task_due_date = dialog.result
+        task_name, task_priority, task_description, task_creation_date, task_due_date = dialog.result
         if task_name:
-            task_list.insert(tk.END, task_name)
+            task_list.insert(tk.END, f"{task_priority} - {task_name}")
             tasks[task_name] = {
+                "priority": task_priority,
                 "description": task_description,
                 "creation_date": task_creation_date,
                 "due_date": task_due_date
@@ -63,23 +64,26 @@ def edit_task(parent):
         if not selected_task_index:
             messagebox.showinfo("Error", "No hay ninguna tarea seleccionada!")
             return
-        task_name = task_list.get(selected_task_index[0])
-        task_data = tasks[task_list.get(selected_task_index[0])]
+        task_entry = task_list.get(selected_task_index[0])
+        task_name = task_entry.split(" - ", 1)[1]
+        task_data = tasks[task_name]  # <-- Aquí está el cambio
+        task_priority = task_data['priority']
         task_description = task_data['description']
         task_creation_date = task_data['creation_date']
         task_due_date = task_data['due_date']
-        dialog = TaskDialog(parent, "Editar tarea", task_name, task_description, task_creation_date, task_due_date)
+        dialog = TaskDialog(parent, "Editar tarea", task_name, task_priority, task_description, task_creation_date, task_due_date)
         if dialog.result:
-            updated_task_name, updated_task_description, updated_task_creation_date, updated_task_due_date = dialog.result
+            updated_task_name, updated_task_priority, updated_task_description, updated_task_creation_date, updated_task_due_date = dialog.result
             if updated_task_name != task_name:
                 del tasks[task_name]
             tasks[updated_task_name] = {
+                'priority': updated_task_priority,
                 'description': updated_task_description,
                 'creation_date': task_creation_date,
                 'due_date': updated_task_due_date
             }
             task_list.delete(selected_task_index[0])
-            task_list.insert(selected_task_index[0], updated_task_name)
+            task_list.insert(selected_task_index[0], f"{updated_task_priority} - {updated_task_name}")
     except KeyError:
         messagebox.showerror("Error", "La tarea seleccionada no fue encontrada!")
         
@@ -90,9 +94,7 @@ def delete_task():
         if not selected_task_index:
             messagebox.showinfo("Error", "No hay ninguna tarea seleccionada!")
             return
-        
-        task_name = task_list.get(selected_task_index[0])
-        
+        task_name = task_list.get(selected_task_index[0]).split(" - ", 1)[1]
         confirm = messagebox.askyesno("Confirmar", f"¿Estás seguro de que vas a eliminar la tarea '{task_name}'?")
         if confirm:
             del tasks[task_name]
